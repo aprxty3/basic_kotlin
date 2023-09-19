8ae14fb85d958fba85ec4f7e31b9dd4471cbca03
aprxty3 <ajiprxty3@gmail.com>
2023-09-19 20:38:31 +0700

    boolean

---
+++ src/main/kotlin/boolean.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    //Conjunction atau AND (&&)
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+
+    val isOpen = if (now >= officeOpen && now <= officeClosed) {
+        true
+    } else {
+        false
+    }
+
+    val isOpen2 = now >= officeOpen && now <= officeClosed
+    val isOpen3 = now in officeOpen..officeClosed
+
+    println("Office is open : $isOpen")
+    println("Office is open : $isOpen2")
+    println("Office is open : $isOpen3")
+
+    //Disjunction atau OR (||)
+    val isClose = now < officeOpen || now > officeClosed
+
+    println("Office is closed : $isClose")
+
+    //Negation atau NOT (!)
+    val isOpen4 = now > officeOpen
+
+    if (!isOpen4) {
+        print("Office is closed")
+    } else {
+        print("Office is open")
+    }
+
+}
