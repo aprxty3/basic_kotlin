8eaaea8b3d6d398c97f63055003c1b947af17c55
aprxty3 <ajiprxty3@gmail.com>
2023-09-19 20:29:47 +0700

    if else expression

---
+++ src/main/kotlin/if_expression.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val openHour = 7
+    val now = 7
+    val office: String
+
+    office = if (now > 7) {
+        "Office already open"
+    } else if (now == openHour) {
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
+
+    println(office)
+}
