6aef369cdda9dea70a313a8804de51558b5c6bb7
aprxty3 <ajiprxty3@gmail.com>
2023-09-20 12:49:47 +0700

    nullable type & using safe calls also elvis operator

---
+++ src/main/kotlin/nullable_type.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val text: String? = null
+
+    if (text != null) {
+        val textLength = text.length
+        println(textLength)
+    }
+
+
+    val text1: String? = null
+    val textLength1 = if (text1 != null) text1.length else 7
+
+
+    val text2: String? = null
+    val textLength2 = text2!!.length
+
+}
