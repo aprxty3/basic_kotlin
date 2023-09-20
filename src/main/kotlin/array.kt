f6dc7b741daf56a9860bba69d407f77288049f99
aprxty3 <ajiprxty3@gmail.com>
2023-09-20 12:41:06 +0700

    array

---
+++ src/main/kotlin/array.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val array = arrayOf(1, 3, 5, 7)
+
+    val mixArray = arrayOf(1, 3, 5, 7, "Ajii", true)
+
+    val intArray = intArrayOf(1, 3, 5, 7)
+
+    println(intArray[2])
+
+    intArray[2] = 11
+    println(intArray[2])
+}
