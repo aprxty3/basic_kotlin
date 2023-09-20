cafe25382c5a5b94fcbea37c58d85dad5c99831b
aprxty3 <ajiprxty3@gmail.com>
2023-09-20 13:02:38 +0700

    string template & intempolaritation

---
+++ src/main/kotlin/string_template.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Ajii"
+    println("Hello my name is $name")
+
+    val old = 20
+    println("I am $name and $old years old")
+
+    val hour = 7
+    println("Office ${if (hour > 7) "already open" else "is closed"}")
+}
