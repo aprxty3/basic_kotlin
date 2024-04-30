c21acc074ea9c29c0ab2f1a1d1bb7e23e6ab9cf8
aji prasetyo <ajiprxty3@gmail.com>
2024-04-30 10:04:32 +0700

    range

--- src/main/kotlin/basic/range.kt
+++ src/main/kotlin/basic/range.kt
@@ -1,3 +1,26 @@
 fun main() {
+    val range1 = 1..100
+    val range2 = 1 until 100
+    val range3 = 100 downTo 1
+
+    println(range1)
+    println(range2)
+    println(range3)
+
+    println(range1.count())
+    println(range1.contains(20))
+    println(range1.contains(200))
+    println(range1.first)
+    println(range1.last)
+    println(range1.step)
+
+    val range4 = 100 downTo 1 step 5
+    println(range4)
+    println(range4.count())
+    println(range4.contains(20))
+    println(range4.contains(200))
+    println(range4.first)
+    println(range4.last)
+    println(range4.step)
 
 }
