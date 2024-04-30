e682beb4f72880ccf5a9d2f6ba63154e2c89d3a2
aji prasetyo <ajiprxty3@gmail.com>
2024-04-30 15:23:27 +0700

    function

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -68,3 +68,5 @@ W
 'src/main/kotlin/basic/break_continue.kt,8/3/831ac542703c5986815d143bbf728ee886de3c4f
 Q
 !src/main/kotlin/basic/function.kt,0/5/05acde49f8b277cb7b9fb5fa9373e5c49870217b
+Z
+*src/main/kotlin/basic/function_argument.kt,b/d/bd870be8d24733e7f790f583f06d4dcd227a8054
--- src/main/kotlin/basic/function.kt
+++ src/main/kotlin/basic/function.kt
@@ -1,4 +1,53 @@
-package basic
+fun main() {
+    helloWorld()
+    sayHello1("Kotlin")
+    println(add(10, 20))
+    sayHello4()
+    sayHello4("World")
+    println(add2(10, 20))
+    sayHello2("Kotlin", 5)
+    sayHello3(null)
+    sayHello3("Kotlin")
+    fullName(lastName = "Kotlin", firstName = "Jetbrains")
+}
 
-class function {
+fun helloWorld() {
+    println("Hello World")
+}
+
+//function with parameter
+fun sayHello1(name: String) {
+    println("Hello $name")
+}
+
+//function with multiple parameter
+fun sayHello2(name: String, age: Int) {
+    println("Hello $name, you are $age years old")
+}
+
+//function with nullable parameter
+fun sayHello3(name: String?) {
+    if (name != null) {
+        println("Hello $name")
+    } else {
+        println("Hello")
+    }
+}
+
+//function with return value
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+//function with default parameter
+fun sayHello4(name: String = "Kotlin") {
+    println("Hello $name")
+}
+
+//function with single expression
+fun add2(a: Int, b: Int): Int = a + b
+
+//function fullName
+fun fullName(firstName: String, lastName: String): String {
+    return "$firstName $lastName"
 }
