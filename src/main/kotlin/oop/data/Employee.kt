2392f086f2e4301ef27a9b83c812122077a811c5
aji prasetyo <ajiprxty3@gmail.com>
2024-05-15 15:16:15 +0700

    function override

--- src/main/kotlin/oop/data/Employee.kt
+++ src/main/kotlin/oop/data/Employee.kt
@@ -1,9 +1,13 @@
 package oop.data
 
 open class Employee(val name: String) {
-    fun sayHello(name: String) {
+    open fun sayHello(name: String) {
         println("Hello $name, My Name are ${this.name} ")
     }
 }
 
-class Manager(name: String) : Employee(name)
+class Manager(name: String) : Employee(name) {
+    override fun sayHello(name: String) {
+        println("Hello $name, My Name are Manager ${this.name} ")
+    }
+}
