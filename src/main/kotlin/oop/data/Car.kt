f75359e7c5a68ac4f6f30f9c94f3ae6db1a958fe
aji prasetyo <ajiprxty3@gmail.com>
2024-05-15 13:17:28 +0700

    init block

--- src/main/kotlin/oop/data/Car.kt
+++ src/main/kotlin/oop/data/Car.kt
@@ -4,4 +4,8 @@ class Car(paramBrand: String, paramYear: Int = 2022) {
 
     var brand: String = paramBrand
     var year: Int = paramYear
+
+    init {
+        println("Car $brand dibuat")
+    }
 }
