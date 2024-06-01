f097d33d892bce475a8d16337114f9916dc24c87
aji prasetyo <ajiprxty3@gmail.com>
2024-06-01 20:39:40 +0700

    hashCode

--- src/main/kotlin/oop/app/Equals.kt
+++ src/main/kotlin/oop/app/Equals.kt
@@ -1,6 +1,17 @@
 package oop.app
 
-class Company(val name: String)
+class Company(val name: String){
+    override fun equals(other: Any?): Boolean {
+        return when(other){
+            is Company -> name == other.name
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
 
 class Name(val name: String) {
     override fun equals(other: Any?): Boolean {
@@ -26,4 +37,8 @@ fun main() {
     val name4 = Name("Kotlin")
 
     println(name3 == name4)
+
+    println(name1.hashCode())
+    println(name2.hashCode())
+    println(name1.hashCode() == name2.hashCode())
 }
